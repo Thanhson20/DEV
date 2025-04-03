1aa05e5dabc654471d3214053f3dfbb24530843c
Thanhson20 <thanhson982003@gmail.com>
2025-04-03 15:49:24 +0700

    jv

---
+++ com.k21cnt2.nts.lesson10/src/main/java/com/k21cnt2/nts/lesson10/Application.java
@@ -0,0 +1,15 @@
+package com.k21cnt2.nts.lesson10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("K21CNT1 - Nguyễn Thành Sơn - 2110900081");
+
+	}
+
+}
---
+++ com.k21cnt2.nts.lesson10/src/test/java/com/k21cnt2/nts/lesson10/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k21cnt2.nts.lesson10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
